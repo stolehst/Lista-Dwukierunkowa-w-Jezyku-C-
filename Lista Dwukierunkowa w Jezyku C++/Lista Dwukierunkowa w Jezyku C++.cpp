29c61da305b9855bce022447d0075aadf6c9481b
stolehst <oleh456yt@gmail.com>
2024-10-28 11:07:20 +0100

    Ustawiamy wskaźniki head

--- Lista Dwukierunkowa w Jezyku C++/Lista Dwukierunkowa w Jezyku C++.cpp
+++ Lista Dwukierunkowa w Jezyku C++/Lista Dwukierunkowa w Jezyku C++.cpp
@@ -10,3 +10,13 @@ struct Node {                                                                   
     Node(int value) : data(value), prev(nullptr), next(nullptr) {}                                                  //Node(int value) ustawia wartosc data na value, 
     //a wskaźniki prev(nullptr) i next(nullptr) na nullptr co oznacza że poczatkowo element nie jest połączony z innymi elementami
 };
+
+class DubleLinkedList {                                                                                             //tworzymy class dla listy
+private:                                                                                                            //w private będą przechowywane head (poczatek listy) i back (koniec listy)
+    Node* head;                                                                                                     //head - poczatek listy
+    Node* back;                                                                                                     //back - koniec listy
+
+public:
+    DubleLinkedList() : head(nullptr), back(nullptr) {                                                              //ustawiamy wskaźniki head i back na nullptr, ponieważ lista jest początkowo pusta
+        srand(static_cast<unsigned>(time(nullptr)));                                                                //ustawiamy tak aby za każdym uruchomieniem programu liczby były inne
+    }
